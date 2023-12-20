9688b99fb1cf31488caad2dbe2d1991e2ce89c6a
James Quintas <43793626+Quintilium@users.noreply.github.com>
2023-12-20 14:01:43 -0500

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,54 +1,53 @@
 import type { NextPage } from "next";
+import styles from "./index.module.css";
 import React from 'react'
 
 const MacBookPro141: NextPage = () => {
   return (
-    <div className="relative bg-midnightblue-100 w-full h-[982px] overflow-hidden text-left text-21xl text-black font-istok-web">
-      <div className="absolute top-[348px] left-[62px] text-[64px] font-inter text-gainsboro [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)]">
-        I’m James Quintas
-      </div>
-      <div className="absolute top-[195px] left-[49px] text-[128px] font-inria-sans text-white">
-        Hello!
-      </div>
-      <div className="absolute top-[0px] left-[960px] bg-gainsboro w-[552px] h-[982px]" />
-      <img
-        className="absolute top-[38px] left-[986px] w-[500px] h-[527.59px] object-cover"
-        alt=""
-        src="/77e777e7-1@2x.png"
-      />
-      <div className="absolute top-[456px] left-[62px] text-5xl text-gainsboro">{`A Freelance IT Specialist & Web Developer`}</div>
-      <div className="absolute top-[637px] left-[960px] bg-midnightblue-200 w-[552px] h-[345px]" />
-      <b className="absolute top-[692px] left-[1034px] text-5xl font-assistant text-white">
-        <p className="m-0">{`I am a professional IT Developer `}</p>
-        <p className="m-0">with experience in multiple</p>
-        <p className="m-0">{`studies of computer based technology, I `}</p>
-        <p className="m-0">work to create innovative solutions</p>
-        <p className="m-0">{`to new tasks and tests that I face in this competitive industry. I work hard to `}</p>
-        <p className="m-0">create easy to use and polished products</p>
+    <div className={styles.macbookPro141}>
+      <div className={styles.imJamesQuintas}>I’m James Quintas</div>
+      <div className={styles.hello}>Hello!</div>
+      <div className={styles.macbookPro141Child} />
+      <img className={styles.e777e71Icon} alt="" src="/77e777e7-1@2x.png" />
+      <div
+        className={styles.aFreelanceIt}
+      >{`A Freelance IT Specialist & Web Developer`}</div>
+      <div className={styles.macbookPro141Item} />
+      <b className={styles.iAmAContainer}>
+        <p className={styles.iAmA}>{`I am a professional IT Developer `}</p>
+        <p className={styles.iAmA}>with experience in multiple</p>
+        <p
+          className={styles.iAmA}
+        >{`studies of computer based technology, I `}</p>
+        <p className={styles.iAmA}>work to create innovative solutions</p>
+        <p
+          className={styles.iAmA}
+        >{`to new tasks and tests that I face in thiscompetitive industry. I work hard to `}</p>
+        <p className={styles.iAmA}>create easy to use and polished products</p>
       </b>
-      <div className="absolute top-[689px] left-[-100px] rounded-[50%] bg-aquamarine w-[696px] h-[408px]" />
+      <div className={styles.macbookPro141Inner} />
       <a
-        className="absolute top-[835px] left-[49px] [text-decoration:underline] font-bold text-[inherit]"
+        className={styles.twitter}
         href="https://twitter.com/Quintilium"
         target="_blank"
       >{`Twitter `}</a>
       <a
-        className="absolute top-[835px] left-[212px] [text-decoration:underline] font-bold text-[inherit]"
+        className={styles.github}
         href="https://github.com/Quintilium"
         target="_blank"
       >
         GitHub
       </a>
       <a
-        className="absolute top-[834px] left-[374px] [text-decoration:underline] font-bold text-[inherit] [text-shadow:0px_4px_4px_rgba(0,_0,_0,_0.25)]"
+        className={styles.linkedin}
         href="https://www.linkedin.com/in/james-quintas-4557a5296/"
         target="_blank"
       >
         Linkedin
       </a>
-      <div className="absolute top-[-73px] left-[-100px] rounded-[50%] bg-aquamarine w-[349px] h-[197px]" />
+      <div className={styles.ellipseDiv} />
       <a
-        className="absolute top-[25px] left-[49px] text-[32px] [text-decoration:underline] font-bold font-literata text-gray"
+        className={styles.resume}
         href="https://docs.google.com/document/d/1kCHOIup6azTlGNarbBLq_YI9RCjOWwhGAL-0WWTGnvM/edit?usp=sharing"
         target="_blank"
       >
